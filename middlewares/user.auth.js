7a6817e14c70fa1fb10f405d940b68178e82239c
Abhishek <Abhishekraut8697@gmail.com>
2024-12-21 23:51:36 +0530

    Setup user and admin middlewares

---
+++ middlewares/user.auth.js
@@ -0,0 +1,26 @@
+const express = require(express);
+const jwt = require("jsonwebtoken");
+
+const handleUserAuth = (req, res, next) => {
+  try {
+    const token = req.cookie.jwt;
+
+    if (!token) {
+      res.status(403).json({
+        message: "Could not varify token please login/signup",
+      });
+    }
+
+    const isTokenValid = jwt.verify(token, process.env.JWT_KEY);
+
+    if (!isTokenValid) {
+      res.status(403).json({
+        message: "Token is invalid please login Again",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = handleUserAuth
