de612958e47af371761d00c35e4f7c5fd60b244b
Abhishek <Abhishekraut8697@gmail.com>
2024-12-22 13:45:26 +0530

    created user signup controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,55 @@
 const express = require("express");
+const {userSignUpSchema} = require('../utils/validationSchema');
+const User = require("../models/user.models");
+const { handleHashPassword } = require("../utils/encryption");
+const jwt = require("jsonwebtoken");
 
-const userSignup = (req, res) => {};
+const userSignup =async (req, res) => {
+    const validationResult = userSignUpSchema.safeParse(req.body);
+
+    if (!validationResult.success) {
+        return res.status(411).json({
+            message: "Invalid input",
+            errors: userRes.error.errors,
+        });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(409).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = handleHashPassword(password);
+
+        // Create user
+        const newUser = await User.create({
+            username,
+            password: hashedPassword,
+        });
+
+        // Generate token
+        const token = jwt.sign({ username }, process.env.JWT_KEY);
+
+        // Set cookie
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        });
+
+        // Send response
+        return res.status(200).json({
+            message: "User Created Successfully",
+        });
+    } catch (error) {
+        next(error); //Pass to global error handler
+    }
+};
 
 const userLogin = (req, res) => {};
 
--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -12,5 +12,9 @@ const courseSchema = z.object({
     imageLink: z.string().url("Invalid image link format"),
 });
 
+const userSignUpSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters long"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+});
 
 module.exports = { adminSignUpSchema , courseSchema};
