55df4610e47b0d04709b20ba37e4221c57b8fb85
Abhishek <Abhishekraut8697@gmail.com>
2024-12-22 13:57:58 +0530

    created userLogin controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ const userSignup =async (req, res) => {
     if (!validationResult.success) {
         return res.status(411).json({
             message: "Invalid input",
-            errors: userRes.error.errors,
+            errors: validationResult.error.errors,
         });
     }
 
@@ -51,7 +51,49 @@ const userSignup =async (req, res) => {
     }
 };
 
-const userLogin = (req, res) => {};
+const userLogin =async (req, res) => {
+    const validationResult = userSignUpSchema.safeParse(req.body);
+
+    if (!validationResult.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+            errors: validationResult.error.errors,
+        });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: "Invalid credentials" });
+        }
+
+        // Verify password
+        const isPasswordValid = await verifyPassword(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        // Generate JWT
+        const token = jwt.sign({ username }, process.env.JWT_KEY);
+
+        // Set cookie
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: parseInt(process.env.COOKIE_MAX_AGE) || 24 * 60 * 60 * 1000, // Default to 24 hours
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
+
+        // Respond
+        res.status(200).json({
+            message: "User logged in successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
 const userLogout = (req, res) => {};
 
